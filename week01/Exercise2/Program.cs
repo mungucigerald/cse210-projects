8c3a0bd3135c24681893081537ea34abff6ae35f
geraldkingucci-star <gmunguci@byupathway.edu>
2026-06-30 00:03:24 +0300

    Exercise 2 done.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your Percentage Score. ");
+        int score = int.Parse(Console.ReadLine());
+        
+        string letter;
+        
+        if (score >= 90)
+        {
+            letter = "A";
+        }
+        else if (score >= 80)
+        {
+            letter = "B";
+        }
+        else if (score >= 70)
+        {
+            letter = "C";
+        }
+        else if (score >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+       
+
+        if (letter != "F")
+        {
+            if (score % 10 >= 7)
+            {
+                letter += "+";
+            }
+            else if (score % 10 <= 3)
+            {
+                letter += "-";
+            }
+        }
+        Console.WriteLine(letter);
+
+
+        if (score >= 70)
+        {
+            Console.WriteLine("You passed this class.");
+        }
+        else
+        {
+            Console.WriteLine("You can do better. Please retake this class.");
+        }
     }
 }
