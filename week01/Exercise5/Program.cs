9b2e874e7a48fe736668b8e8ab2cef7e533b6ca0
geraldkingucci-star <gmunguci@byupathway.edu>
2026-07-01 19:23:05 +0300

    Exercise 5 done

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squareNumber = SquareNumber(number);
+        DisplayMessage(name, squareNumber);
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+    Console.Write("Please enter your name: ");
+    string userName = Console.ReadLine();
+    return userName;
+    }
+        
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int userNumber = int.Parse(Console.ReadLine());
+        return userNumber;
+    }
+
+    static int SquareNumber(int number)
+    {
+    int square = number * number;
+    return square;
+    }
+
+    static void DisplayMessage(string userName, int squareNumber)
+    {
+        Console.WriteLine($" {userName}, the square of your number is {squareNumber}");
+    }
+
 }
